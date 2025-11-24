51b877dee7f8cff0042aeaadfc54acf5569aeace
soohyunkyung <113413847+soohyunkyung@users.noreply.github.com>
2025-11-24 13:13:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,14 @@ st.set_page_config( # 페이지 설정
     } # 딕셔너리 끝 }
 )
 
-st.sidebar.title('다양한 사이드바 위젯들')
+# st.sidebar.title('다양한 사이드바 위젯들')
 
-st.sidebar.checkbox('외국인 포함')
-st.sidebar.checkbox('고령인구 포함')
-st.sidebar.divider() # ┳ 구분선
-st.sidebar.radio('데이터 타입', ['전체', '남성', '여성'])
-st.sidebar.slider('나이', 0, 100, (20, 50))
-st.sidebar.selectbox('지역', ['서울', '경기', '인천', '대전', '대구', '부산', '광주'])
+# st.sidebar.checkbox('외국인 포함')
+# st.sidebar.checkbox('고령인구 포함')
+# st.sidebar.divider() # ┳ 구분선
+# st.sidebar.radio('데이터 타입', ['전체', '남성', '여성'])
+# st.sidebar.slider('나이', 0, 100, (20, 50))
+# st.sidebar.selectbox('지역', ['서울', '경기', '인천', '대전', '대구', '부산', '광주'])
+
 
 
